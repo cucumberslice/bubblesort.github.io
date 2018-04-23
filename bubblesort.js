03e9f2e5e13b82073fc80f6265e4e8013d8bb0c5
Yeshi Wangdi <ywblackberry@gmail.com>
2018-04-23 13:09:09 -0400

    js file

---
+++ bubblesort.js
@@ -0,0 +1,22 @@
+let swapCount;
+    
+function bubbleSort(arr) {
+
+    if (swapCount === 0) {
+        return arr;
+    } else {
+        swapCount = 0;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] < arr[i]) {
+                let temp = arr[i]
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapCount++;
+            }
+        }
+        return bubbleSort(arr);
+    }
+}
+
+
+//bubbleSort([2, 1, 6, 3, 10, 4, 20, 5])
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,13 @@ describe('Bubble Sort', function(){
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
+
+  it('handles mulitple items array', function(){
+      expect( bubbleSort([5,2,7])).toEqual([2,5,7]);
+  });
+
+  it('handles single items array', function(){
+
+    expect( bubbleSort([1])).toEqual([1]);
+  });
 });
